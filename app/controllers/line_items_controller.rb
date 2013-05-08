812fd96864d087b54cebc00b4776d6481080f446
Christine Moy <christinemoy@gmail.com>
2013-05-07 22:45:29 -0400

    Added quantity column to line_item

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -42,8 +42,9 @@ class LineItemsController < ApplicationController
   def create
     @cart = current_cart
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build
-    @line_item.product = product
+    @line_item = @cart.add_product(product.id)
+    #@line_item = @cart.line_items.build
+    #@line_item.product = product
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.'}
