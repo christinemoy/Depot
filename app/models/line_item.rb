3362e2ddac4e5301cd6388a9e55bc918b56b4371
Christine Moy <christinemoy@gmail.com>
2013-05-07 23:21:33 -0400

    Cart display with total

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,9 @@ class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id, :quantity
   belongs_to :product
   belongs_to :cart
+
+  def total_price
+  	product.price * quantity
+  end
+  
 end
