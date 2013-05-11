8b06e033a799c01f6ed93e9424084727a6016ccb
Christine Moy <christinemoy@gmail.com>
2013-05-11 15:24:42 -0400

    Prevent last admin deletion

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, uniqueness: true
   has_secure_password
+
+  after_destroy :ensure_an_admin_remains
+
+  private
+
+  	def ensure_an_admin_remains
+  		if User.count.zero?
+  			raise "Cannot delete last user"
+  		end
+  	end
+  	
 end
