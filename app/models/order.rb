ef85fdea71aa25f74ea1a30023ef4e8e36bfb641
Christine Moy <christinemoy@gmail.com>
2013-05-11 08:35:55 -0400

    Change line item test to product: and order:

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ActiveRecord::Base
   attr_accessible :address, :email, :name, :pay_type
   PAYMENT_TYPES = ["Check", "Credit Cart", "Purchase Order"]
+
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
+  
 end
