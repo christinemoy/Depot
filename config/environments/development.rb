5707175df644e7a98f1af83fcdf2857ac62840da
Christine Moy <christinemoy@gmail.com>
2013-05-11 10:54:25 -0400

    Add text email sender

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,15 @@
 Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    user_name: "christinemoy",
+    password: "Linden79",
+    enable_starttls_auto: true
+  }
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
