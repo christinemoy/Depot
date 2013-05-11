cd97f0c0b9509e3d91d5c0adfc6d8ef12a2dd863
Christine Moy <christinemoy@gmail.com>
2013-05-11 08:01:18 -0400

    Add product and cart to attr accessible; no test errors

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,5 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id, :quantity
+  attr_accessible :cart_id, :product_id, :quantity, :cart, :product
   belongs_to :product
   belongs_to :cart
 
