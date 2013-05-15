6e2d144e2e78bf542e0d5a469d46918e0b7a7b54
Christine Moy <christinemoy@gmail.com>
2013-05-15 14:41:58 -0400

    Add SendGrid credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,16 @@
 Depot::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
+  config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.smtp_settings = {
+    address: "smtp.heroku.com",
+    port: 587,
+    domain: "heroku.com",
+    authentication: "plain",
+    user_name: "app15624147",
+    password: "lzkwgrx3",
+    enable_starttls_auto: true
+  }
   # Code is not reloaded between requests
   config.cache_classes = true
 
