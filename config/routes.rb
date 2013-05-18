92912fb9abed09c5b6a74d2893535961d8de32b6
Christine Moy <christinemoy@gmail.com>
2013-05-17 22:58:11 -0400

    Update route to start at Discover

--- config/routes.rb
+++ config/routes.rb
@@ -76,7 +76,7 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'store#index', as: 'store'
+  root :to => 'static_pages#discover', as: 'discover'
 
   # See how all your routes lay out with "rake routes"
 
