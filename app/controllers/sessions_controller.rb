e2fc714412d9ba5beb55ab9b04f2758b5bd22257
Christine Moy <christinemoy@gmail.com>
2013-05-17 23:46:32 -0400

    Fix session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
   	session[:user_id] = nil
-  	redirect_to store_url, notice: "Logged out"
+  	redirect_to order_url, notice: "Logged out"
   end
 end
